7189f1620728143af8c048161f7fd997f613b694
Danielle Fernandes <daniramonaa@gmail.com>
2019-12-10 19:24:22 -0300

    Todos repositórios do curso de React Woman Can Code

---
+++ frontend/frontend/src/components/header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../assets/styles/components/header/header.css'
+
+
+function index() {
+  return (
+    <h1>MIGAS TAMO NO HEADER</h1>
+  );
+}
+
+export default index;
